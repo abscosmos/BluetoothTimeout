77121cb68f582b98a26d11b956aa604959089328
abscosmos <joshua@dinoslice.com>
2025-05-17 16:12:08 -0500

    Load icon

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ mod timeout;
 mod logging;
 mod notification;
 
-use std::str::FromStr;
 use eframe::egui::ViewportBuilder;
+use eframe::icon_data;
 use tokio::sync::mpsc;
 use app::BluetoothApp;
 
@@ -20,14 +20,16 @@ async fn main() -> eframe::Result {
     
     logging::init(tx).expect("init shouldn't fail");
     
+    let icon = icon_data::from_png_bytes(include_bytes!("../assets/icon.png")).expect("png bytes should be valid");
+    
     eframe::run_native(
         "Bluetooth Timeout",
         eframe::NativeOptions {
             viewport: ViewportBuilder::default()
                 .with_resizable(false)
                 .with_maximize_button(false)
-                .with_inner_size((250.0, 275.0)),
-                // .with_icon()
+                .with_inner_size((250.0, 275.0))
+                .with_icon(icon),
                 .. Default::default()
         },
         Box::new(move |cc| {
